e3dd4160c36d9b9430e3eb431b3d276cb61ca030
Mengyao <wanzhijane@qq.com>
2023-02-14 21:54:21 -0500

    gtfs read

--- Codes/public_transit.R
+++ Codes/public_transit.R
@@ -10,6 +10,31 @@ jma_county = c("031", "019", "003", "089", "109")
 
 jma_tracts <- tracts(state = "FL", county = jma_county)
 
-jma_rta_gtfs <- here("data",
-                     "jma_gtfs1.zip") %>%
-  read_gtfs()
+jma_gtfs <- read_gtfs("/Users/limengyao/Desktop/R/tbf_jacksonville/data/jta-gtfs.zip")
+
+route_shape <- shapes_as_sf(jma_gtfs$shapes)
+
+ggplot() +
+  geom_sf(data = jma_tracts,
+          fill = "cornsilk", color = "gray", size = 0.3) +
+  geom_sf(data = route_shape,
+          aes(color = shape_id)) +
+  theme_map() +
+  theme(legend.position = "none")
+
+ggsave("images/public_transit_map.jpg", height = 4.25, width = 6.5, units = "in")
+
+#### Importing Excel sheets into R
+library(realxl)
+
+IVTT <- read_xlsx("data/jta-rta-skims.xlsx", sheet = "In-Vehicle Time") 
+
+
+
+
+
+
+
+
+
+
